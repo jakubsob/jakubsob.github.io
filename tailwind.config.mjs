6abeb0f61ce8f9e7edc27f38a11c60fedbd37220
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-10-29 18:33:05 +0100

    💄 override sky color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,60 +2,79 @@ const plugin = require('tailwindcss/plugin')
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
       fontFamily: {
-        sans: ['Geist Sans', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-        mono: ['Geist Mono', 'monospace'],
-        code: ['Fira Code', 'monospace'],
-        syne: ['Syne Variable', 'sans-serif'],
-        monoton: ['Monoton', 'serif'],
+        sans: ["Geist Sans", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+        mono: ["Geist Mono", "monospace"],
+        code: ["Fira Code", "monospace"],
+        syne: ["Syne Variable", "sans-serif"],
+        monoton: ["Monoton", "serif"],
+      },
+      colors: {
+        sky: {
+          DEFAULT: "#262B36",
+          50: "#BDC3D1",
+          100: "#B1B8C8",
+          200: "#99A3B7",
+          300: "#818DA7",
+          400: "#697796",
+          500: "#59647E",
+          600: "#485166",
+          700: "#373E4E",
+          800: "#262B36",
+          900: "#0F1115",
+          950: "#030405",
+        },
       },
     },
-	},
+  },
   plugins: [
-    require('@tailwindcss/typography'),
-    plugin(function({ addBase, theme }) {
+    require("@tailwindcss/typography"),
+    plugin(function ({ addBase, theme }) {
       addBase({
-        'h1': {
-          fontFamily: theme('fontFamily.uppercase'),
-          fontFamily: theme('fontFamily.syne'),
-          fontWeight: theme('fontWeight.semibold'),
-          fontSize: theme('fontSize.2xl')
+        h1: {
+          fontFamily: theme("fontFamily.uppercase"),
+          fontFamily: theme("fontFamily.syne"),
+          fontWeight: theme("fontWeight.semibold"),
+          fontSize: theme("fontSize.2xl"),
         },
-        'h2': {
-          fontFamily: theme('fontFamily.uppercase'),
-          fontFamily: theme('fontFamily.syne'),
-          fontSize: theme('fontSize.xl')
+        h2: {
+          fontFamily: theme("fontFamily.uppercase"),
+          fontFamily: theme("fontFamily.syne"),
+          fontSize: theme("fontSize.xl"),
         },
-        'h3': {
-          fontSize: theme('fontSize.lg')
+        h3: {
+          fontSize: theme("fontSize.lg"),
         },
-      })
+      });
     }),
-    plugin(function({ addUtilities, theme }) {
-      function extractVars (obj, group = '', prefix) {
+    plugin(function ({ addUtilities, theme }) {
+      function extractVars(obj, group = "", prefix) {
         return Object.keys(obj).reduce((vars, key) => {
           const value = obj[key];
-          const cssVariable = key === "DEFAULT" ? `--${prefix}${group}` : `--${prefix}${group}-${key}`;
+          const cssVariable =
+            key === "DEFAULT"
+              ? `--${prefix}${group}`
+              : `--${prefix}${group}-${key}`;
 
           const newVars =
-          typeof value === 'string'
-          ? { [cssVariable]: value }
-          : extractVars(value, `-${key}`, prefix);
+            typeof value === "string"
+              ? { [cssVariable]: value }
+              : extractVars(value, `-${key}`, prefix);
 
           return { ...vars, ...newVars };
         }, {});
       }
 
       addUtilities({
-        ':root': {
-          ...extractVars(theme('colors'), '', 'color'),
-          ...extractVars(theme('boxShadow'), '', 'box-shadow')
-        }
+        ":root": {
+          ...extractVars(theme("colors"), "", "color"),
+          ...extractVars(theme("boxShadow"), "", "box-shadow"),
+        },
       })
-    })
+    }),
   ],
-}
+};
