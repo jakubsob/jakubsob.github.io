52ebfc5e44d3dfe9486b040cb8f1344c68e775ca
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-11-07 19:55:22 +0100

    ✨ add a landing page

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Header({ children }) {
+  useEffect(() => {
+    gsap.from("#header", {
+      scrollTrigger: {
+        trigger: "#blog",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        toggleActions: "play none none reverse",
+      },
+      color: "var(--color-marine)",
+    });
+  }, []);
+
+  return (
+    <div
+      id="header"
+      className="w-full fixed top-0 z-10 h-12 backdrop-blur-sm text-black"
+    >
+      <header className="max-w-[65ch] mx-auto px-2 py-2 tracking-wider">
+        <nav className="flex flex-row items-center justify-start">
+          {children}
+        </nav>
+      </header>
+    </div>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,6 +13,9 @@ export default {
         syne: ["Syne Variable", "sans-serif"],
         monoton: ["Monoton", "serif"],
       },
+      fontSize: {
+        clamp: "clamp(2rem, 5vw, 3rem)",
+      },
       colors: {
         sky: {
           DEFAULT: "#262B36",
@@ -27,6 +30,34 @@ export default {
           800: "#262B36",
           900: "#0F1115",
           950: "#030405",
+        },
+        malibu: {
+          DEFAULT: "#82B1FF",
+          50: "#E8F1FF",
+          100: "#D4E4FF",
+          200: "#ABCAFF",
+          300: "#82B1FF",
+          400: "#4A8EFF",
+          500: "#126BFF",
+          600: "#0051D9",
+          700: "#003CA1",
+          800: "#002768",
+          900: "#001230",
+          950: "#000814",
+        },
+        marine: {
+          DEFAULT: "#77C0CA",
+          50: "#FBFDFD",
+          100: "#ECF6F8",
+          200: "#CFE9EC",
+          300: "#B2DBE1",
+          400: "#94CED5",
+          500: "#77C0CA",
+          600: "#4FADBA",
+          700: "#3B8B96",
+          800: "#2B666E",
+          900: "#1B4046",
+          950: "#132E31",
         },
       },
     },
@@ -74,7 +105,7 @@ export default {
           ...extractVars(theme("colors"), "", "color"),
           ...extractVars(theme("boxShadow"), "", "box-shadow"),
         },
-      })
+      });
     }),
   ],
 };
