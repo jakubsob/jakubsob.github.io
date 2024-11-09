6bb4882136fdb5a7f581c1304191bab861983c20
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-11-09 17:50:43 +0100

    💄 fix header items alignment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,7 +24,7 @@ export default function Header({ children }) {
       className="w-full fixed top-0 z-10 h-12 backdrop-blur-sm text-black"
     >
       <header className="max-w-[65ch] mx-auto px-2 py-2 tracking-wider">
-        <nav className="flex flex-row items-center justify-start">
+        <nav className="flex flex-row gap-4 items-center justify-start">
           {children}
         </nav>
       </header>
