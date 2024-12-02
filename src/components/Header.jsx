2d4092d46ef15e99105e40d119d4fd6a171663c4
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-12-02 19:25:05 +0100

    💄 add parallax to hero section image and scroll snap

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header({ children }) {
   return (
     <div
       id="header"
-      className="w-full fixed top-0 z-10 h-12 backdrop-blur-sm text-black"
+      className="w-full fixed top-0 z-10 h-12 backdrop-blur-sm text-black transition-colors duration-500"
     >
       <header className="max-w-[65ch] mx-auto px-2 py-2 tracking-wider">
         <nav className="flex flex-row gap-4 items-center justify-start">
