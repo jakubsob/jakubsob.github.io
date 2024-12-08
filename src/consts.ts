0a842026aeb6bd86e7a29bb9e958c966f317eb6a
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-12-08 21:05:33 +0100

    🔧 update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'jakub::sobolewski';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Building quality by testing';
 export const R_BLOG_DESCRIPTION = 'All things R/Shiny and beyond.'
