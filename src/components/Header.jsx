f37c2c72d7b617e4e433e7c41bc15095e41e0c48
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-12-13 20:26:33 +0100

    💄 style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,16 @@ export default function Header({ children }) {
   return (
     <div
       id="header"
-      className="w-full fixed top-0 z-10 h-12 backdrop-blur-sm text-black transition-colors duration-500"
+      className="fixed top-0 z-10 h-12 w-full
+    text-black transition-all duration-500"
     >
-      <header className="max-w-[65ch] mx-auto px-2 py-2 tracking-wider">
+      <header
+        className="max-w-[65ch]
+        mx-auto mt-2
+        px-4 py-4
+        tracking-wider
+        rounded-lg"
+      >
         <nav className="flex flex-row gap-4 items-center justify-start">
           {children}
         </nav>
