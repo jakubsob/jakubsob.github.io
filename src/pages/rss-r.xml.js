bcce679bb22b178d6b6b1d2ddc433cbd80b5dbe2
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2025-01-11 12:15:07 +0100

    ✨ improve rss feed

--- src/pages/rss-r.xml.js
+++ src/pages/rss-r.xml.js
@@ -1,19 +1,42 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
+import { loadRenderers } from "astro:container";
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.filter((post) => post.data.tags.includes('r')).map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-    xmlns: {
-      atom: 'http://jakubsob.github.io/rss-r.xml',
-    },
-	});
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+  const posts = await getCollection("blog");
+
+  const filteredPosts = posts
+    .filter((post) => !post.data.draft)
+    .filter((post) => post.data.tags.includes("r"));
+
+  const items = await Promise.all(
+    filteredPosts.map(async (post) => {
+      const { Content } = await post.render();
+      const content = await container.renderToString(Content);
+      const link = new URL(`/blog/${post.slug}`, context.url.origin).toString();
+      return {
+        ...post,
+        title: post.data.title,
+        description: post.data.description,
+        site: context.site,
+        link,
+        content,
+        pubDate: post.data.pubDate,
+      };
+    })
+  );
+
+  items.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items,
+  });
 }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,40 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { experimental_AstroContainer as AstroContainer } from "astro/container";
+import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
+import { loadRenderers } from "astro:container";
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const renderers = await loadRenderers([getMDXRenderer()]);
+  const container = await AstroContainer.create({ renderers });
+  const posts = await getCollection("blog");
+
+  const filteredPosts = posts.filter((post) => !post.data.draft);
+
+  const items = await Promise.all(
+    filteredPosts.map(async (post) => {
+      const { Content } = await post.render();
+      const content = await container.renderToString(Content);
+      const link = new URL(`/blog/${post.slug}`, context.url.origin).toString();
+      return {
+        ...post,
+        title: post.data.title,
+        description: post.data.description,
+        site: context.site,
+        link,
+        content,
+        pubDate: post.data.pubDate,
+      };
+    })
+  );
+
+  items.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
+
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items,
+  });
 }
