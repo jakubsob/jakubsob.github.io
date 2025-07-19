f31d2e120d28b35ba624c072e86f7925dd004283
jakub <jakupsob@gmail.com>
2025-07-19 19:12:30 +0200

    feat: :sparkles: add motion

--- src/components/ui/animate.tsx
+++ src/components/ui/animate.tsx
@@ -10,6 +10,8 @@ const animateVariants = cva(
     variants: {
       variant: {
         default: "intersect-once intersect:motion-opacity-in-0 intersect:motion-translate-y-in-25",
+        header: "motion-preset-slide-up-lg md:motion-duration-1000",
+        fadeIn: "motion-preset-fade-lg",
       },
     },
     defaultVariants: {
@@ -22,14 +24,18 @@ export interface AnimateProps
   extends React.HTMLAttributes<HTMLDivElement>,
     VariantProps<typeof animateVariants> {
   asChild?: boolean
+  delay?: number | string
 }
 
 const Animate = React.forwardRef<HTMLDivElement, AnimateProps>(
-  ({ variant, asChild = false, ...props }, ref) => {
+  ({ variant, delay, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "div"
     return (
       <Comp
-        className={cn(animateVariants({ variant }))}
+        className={cn(
+          animateVariants({ variant }),
+          delay !== undefined && `motion-delay-${delay}`
+        )}
         ref={ref}
         {...props}
       />
