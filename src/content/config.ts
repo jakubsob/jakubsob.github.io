b97bdd25f6d050a3f5f21b7daabb2c195018d6ce
jakub <jakupsob@gmail.com>
2025-07-24 09:36:29 +0200

    feat: :safety_vest: validate post tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
+// Define allowed tags
+const allowedTags = [
+  'bdd',
+  'cucumber',
+  'llm',
+  'plumber',
+  'r',
+  'resource',
+  'shiny',
+  'shinytest2',
+  'tdd',
+  'tests',
+  'ui',
+] as const;
+
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
@@ -12,7 +27,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
-    tags: z.array(z.string()),
+    tags: z.array(z.enum(allowedTags)),
     draft: z.boolean().optional(),
 	}),
 });
